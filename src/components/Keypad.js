6dcce0bae6cc8f5b3df9b3ea3ca4b2071714c64b
Patrobas Bwire <patrobasbwire@gmail.com>
2022-09-21 13:54:56 +0300

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function keypadFn (eventFunction){
+        console.log(eventFunction.target.name);
+    }
     return (
-        <div></div>
-    )
+        <div>
+            <input
+          type="password"
+          name="Entering password..."
+          onChange={keypadFn}
+          placeholder="Enter your password..."
+        />
+        </div>
+    );
 }
 
 export default Keypad;
